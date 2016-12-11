8d02952f49228a97349ac309aba642de81d0ed53
lloydliu717 <lloydliu@bu.edu>
2016-12-11 10:50:59 -0500

    2

---
+++ global.R
@@ -0,0 +1,32 @@
+Saturdaytweets <- readRDS("tweets.RDS")
+Sundaytweets <- readRDS("tweetsSun.RDS")
+#drop useless column
+cleantweets <- function(tweets.df){
+  tweets.df[,c("favorited","retweet_count",
+               "source","retweeted","in_reply_to_screen_name",
+               "geo_enabled","protected","user_url","place_name",
+               "place_id","expanded_url","url"):=NULL]
+  tweets.df$text <- iconv(tweets.df$text, from = "latin1", to = "ASCII", sub="")
+  tweets.df$text <- gsub('http\\S+\\s*', '', tweets.df$text)
+  tweets.df$location <- iconv(tweets.df$location, from = "latin1", to = "ASCII", sub="")
+  tweets.df$description <- iconv(tweets.df$description, from = "latin1", to = "ASCII", sub="")
+  tweets.df[,c("Weekday","Date1","Date2","time","timezone","year"):=tstrsplit(created_at, " ", fixed=TRUE)]
+  tweets.df[,Date1 := paste(Date1,Date2)]
+  tweets.df[,EAST_Time := times(time)-times("05:00:00")]
+  tweets.df[,c("Date2","timezone","time"):=NULL]
+  return(tweets.df)
+}
+Saturdaytweets <- cleantweets(Saturdaytweets)
+Sundaytweets <- cleantweets(Sundaytweets)
+
+selectdata_list <- c("Saturday","Sunday")
+selectfocus_list <- c("lang","followers_count")
+
+
+irisdata <- read.csv("irisdata.csv")
+
+selectx_list <- colnames(irisdata)
+selecty_list <- colnames(irisdata)
+
+class(selecty_list)
+class(selectfocus_list)
---
+++ ui.R
@@ -0,0 +1,74 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+
+shinyUI(
+  fluidPage(
+    theme="bootstrap.css",
+    titlePanel("Who, What and Where on Twitter at weekend's Midnight",windowTitle = "Twitter Shiny"),
+    navbarPage(title="Contents",
+               
+               tabPanel("Introduction & Data preparation",
+               p("This is a Shiny App of my final project in MA615."),
+               p("In this project, I combine mining a social networking service (twitter), mapping, document creation, and shiny."),
+               a(href="https://github.com/lloydliu717/MA615_Final_Assignment","View code of this project on Github"),
+               p("WE WILL PUT A WORD CLOUD HERE"),
+               hr(),#make dividing line
+               #br(),#start new line
+               code("#see code on Github Final_assignment.Rmd"),
+               
+               hr(),
+               p("Author: Lloyd Liu, Christian Sun"),
+               splitLayout(
+                 img(src="4.pic.jpg"),
+                 img(src="3.pic.jpg"))
+               
+               ),
+               
+               tabPanel("WHO",
+                        sidebarLayout(
+                          sidebarPanel(selectInput(inputId="selectdata",
+                                                   label="Which Day",
+                                                   choices=selectdata_list),
+                                       selectInput(inputId="selectfocus",
+                                                   label="Focus",
+                                                   choices=selectfocus_list),
+                                       actionButton(inputId='getplot',
+                                                    label="Give mt the plot")),
+                          mainPanel(plotOutput("whoplot"))
+                          )
+                        
+                        ),
+               
+               tabPanel("WHAT",
+                        checkboxGroupInput(inputId="cbg",
+                                           label="checkbox group",
+                                           choices=c("A","B","C")),
+                        textOutput("outputcbg"),
+                        radioButtons(inputId="cb",
+                                     label="radio buttons",
+                                     choices=c("a","b","c"),
+                                     selected="a")),
+               
+               tabPanel("WHERE",
+                        sidebarLayout(
+                          sidebarPanel(selectInput(inputId="selectx",
+                                                   label="X Axis",
+                                                   choices=selectx_list),
+                                       selectInput(inputId="selecty",
+                                                   label="Y Axis",
+                                                   choices=selecty_list),
+                                       actionButton(inputId='getplot',
+                                                    label="Give mt the plot")),
+                          mainPanel(plotOutput("irisplot"))
+                          )
+                        )
+               )
+    )
+  )
